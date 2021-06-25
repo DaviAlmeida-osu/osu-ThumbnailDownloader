973c2a3c7ce542a5053530673afc72b573da38f2
Davi Almeida <73702909+DaviAlmeida-osu@users.noreply.github.com>
2021-06-25 13:52:25 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ if os.stat(f"{c}\keyapi.txt").st_size == 0:
     ak = input('Insert your api key here, it will be saved on your machine for future uses: ')
     with open(f"{c}\keyapi.txt.", 'w') as a:
         a.write(ak)
+        
 apikey = open(f"{c}\keyapi.txt", 'r')
 get_map(int(beatmapid), apikey.read())
 download_bg(int(beatmapid), c)
